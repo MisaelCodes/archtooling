19a44bb4087fcb7c78078c82316b0bfc97cf3b3e
MisaelCodes <misacodes@gmail.com>
2025-07-21 08:58:43 -0500

    [add] added some other wallpaper engines

--- instalations_arch.sh
+++ instalations_arch.sh
@@ -4,24 +4,32 @@ sudo pacman -S fastfetch
 sudo pacman -S podman podman-compose --noconfirm
 sudo pacman -S go --noconfirm
 
+# Audio
+sudo pacman -S pipewire wireplumber
+sudo pacman -S pipewire-docs
+sudo pacman -S pavucontrol
+echo "Remember to set the default audio device"
+
 # Hyperland Utils
 sudo pacman -S gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc
 sudo pacman -S dunst libnotify
-sudo pacman -S pipewire wireplumber
 sudo pacman -S xdg-desktop-portal-hyprland
 sudo pacman -S qt5-wayland qt6-wayland
 sudo pacman -S ttf-nerd-fonts-symbols
 sudo pacman -S hyprpolkitagent
 sudo pacman -S waybar
 
+# Hyperland Wallpaper
+sudo pacman -S hyprpaper
+echo "Create or edit the hyprpaper.conf for preload and wallpaper images!"
+sudo pacman -S sway # another lightweight wallpaper engine
+sudo pacman -S swww # another wallpaper engine
+
+
 # Bluetooth
 sudo pacman -S bluez
 sudo pacman -S bluez-utils
 sudo pacman -S blueman
-
-# Audio
-sudo pacman -S pipewire-docs
-sudo pacman -S pavucontrol
 
 # Clipboard
 sudo pacman -S wl-clipboard
@@ -53,4 +61,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Remember to add the theme powerlevel10k/powerlevel10k and the plugins zsh-autosuggestions zsh-syntax-highlighting"
 
-
+# VNC
+sudo pacman -S remmina
+sudo pacman -S gtk-vnc gtk-vnc-docs
